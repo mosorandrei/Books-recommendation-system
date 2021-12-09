d7254c84b95084a7b8e67111081eba4887482b93
ioanapelinn <75810645+ioanapelinn@users.noreply.github.com>
2021-12-10 00:54:46 +0200

    Added admin pages and user discover page

--- FE/src/components/avatar/avatar.js
+++ FE/src/components/avatar/avatar.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import './avatar.scss';
+import React from "react";
+import "./avatar.scss";
 
-import defaultAvatar from '../../assets/default-avatar.png';
+import defaultAvatar from "../../assets/default-avatar.png";
 
-function Avatar({
-    avatarLink,
-    name = "John Doe"
-}) {
-    return (
-        <div className="information">
-            <img className="avatar" src={avatarLink || defaultAvatar} />
-            <div>
-                <p className="name">{name}</p>
-            </div>
-        </div>
-    );
+function Avatar({ avatarLink, name = "John Doe", username = "" }) {
+  return (
+    <div className="information">
+      <img className="avatar" src={avatarLink || defaultAvatar} />
+      <div>
+        <p className="name">{name}</p>
+        {username && <p className="username"> {username} </p>}
+      </div>
+    </div>
+  );
 }
-
-
 
 export default Avatar;
